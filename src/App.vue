80733df3c95900ff216a7587500a91290f7a7a5a
Matthew The <matthew.the@tum.de>
2022-03-20 11:37:59 +0100

    First axios call

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <ProteinCorrelation/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import ProteinCorrelation from './components/ProteinCorrelation';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    ProteinCorrelation,
   },
 
   data: () => ({
---
+++ src/components/ProteinCorrelation.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'ProteinCorrelation',
+
+    data: () => ({
+      
+    }),
+
+    mounted() {
+        axios
+            .get("https://www.proteomicsdb.org/proteomicsdb/logic/api_v2/api.xsodata/Protein?$filter=GENE_NAME eq 'EGFR'&$format=json")
+            .then(response => {
+              console.log(response)
+            })
+            .catch(error => {
+              console.log(error)
+            })
+    }
+}
+</script>
