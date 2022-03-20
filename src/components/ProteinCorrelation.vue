c13599267f0894b2c73a7f82b3da9a17fbb763e5
Matthew The <matthew.the@tum.de>
2022-03-20 12:06:13 +0100

    Form to retrieve protein id

--- src/components/ProteinCorrelation.vue
+++ src/components/ProteinCorrelation.vue
@@ -2,6 +2,15 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
+        <v-text-field 
+            v-model="geneName"
+            label="Gene name"
+            placeholder="EGFR"
+        ></v-text-field>
+        <v-btn
+            @click="updateProteinId">
+            Get Protein ID
+        </v-btn>
         {{ proteinId }}
       </v-col>
     </v-row>
@@ -15,26 +24,35 @@ export default {
     name: 'ProteinCorrelation',
 
     data: () => ({
+      geneName: "",
       proteinId: ""
     }),
+    
+    methods: {
+        updateProteinId: function() {
+            axios
+                .get("https://www.proteomicsdb.org/proteomicsdb/logic/api_v2/api.xsodata/Protein", 
+                    { params : { '$filter' : `GENE_NAME eq '${this.geneName}' and DATABASE eq 'sp' and PARENT_PROTEIN_ID eq PROTEIN_ID and DECOY eq 0`,
+                                 '$select' : 'PROTEIN_ID', 
+                                 '$format' : 'json' } 
+                    }
+                )
+                .then(response => {
+                  console.log(response.data.d.results)
+                  if (response.data.d.results.length > 0) {  
+                    this.proteinId = response.data.d.results[0].PROTEIN_ID
+                  } else {
+                    this.proteinId = -1
+                  }
+                })
+                .catch(error => {
+                  console.log(error)
+                })
+        }
+    },
 
     mounted() {
-        let geneName = 'EGFR'
-        axios
-            .get("https://www.proteomicsdb.org/proteomicsdb/logic/api_v2/api.xsodata/Protein", 
-                { params : { '$filter' : `GENE_NAME eq '${geneName}' and DATABASE eq 'sp' and PARENT_PROTEIN_ID eq PROTEIN_ID and DECOY eq 0`,
-                             '$select' : 'PROTEIN_ID', 
-                             '$format' : 'json' } 
-                }
-            )
-            .then(response => {
-              if (response.data.d.results.length > 0) {
-                this.proteinId = response.data.d.results[0].PROTEIN_ID
-              }
-            })
-            .catch(error => {
-              console.log(error)
-            })
+        
     }
 }
 </script>
