95dfed50584d7a8f4bb266f67762619dd533c46a
Matthew The <matthew.the@tum.de>
2022-03-22 15:50:20 +0100

    Retrieve protein expressions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,30 @@
         placeholder="EGFR"
         @change="updateProteinId"
      ></v-text-field>
-     <span>{{ proteinId1 }}</span>
+     <span>Protein id 1: {{ proteinId1 }}</span>
+     <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">
+                Sample ID
+              </th>
+              <th class="text-left">
+                Expression
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="item in proteinExpressions1"
+              :key="item.SAMPLE_ID"
+            >
+              <td>{{ item.SAMPLE_ID }}</td>
+              <td>{{ item.EXPRESSION }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
   </v-container>
 </template>
 
@@ -17,12 +40,17 @@ export default {
     name: 'ProteinCorrelation',
     data: () => ({
       geneName1: "",
-      proteinId1: ""
+      proteinId1: "",
+      proteinExpressions1: []
     }),
     methods: {
         updateProteinId() {
-            this.getProteinId(this.geneName1).then(response => {
+            let promise1 = this.getProteinId(this.geneName1).then(response => {
                 this.proteinId1 = response
+                return this.getProteinExpressions(this.proteinId1)
+            })
+            promise1.then(response => {
+                this.proteinExpressions1 = response
             })
         },
         getProteinId(geneName) {
@@ -44,6 +72,25 @@ export default {
                   console.log(error)
                 })
         },
-    }
+        getProteinExpressions(proteinId) {
+            let proteinExpressions = []
+            return axios
+                .get(`https://www.proteomicsdb.org/proteomicsdb/logic/api_v2/api.xsodata/Protein(${proteinId})/ProteinExpression`,
+                    { params : { '$filter' : 'CALCULATION_METHOD eq 0',
+                                 '$select' : 'SAMPLE_ID,EXPRESSION',
+                                 '$format' : 'json' }
+                    }
+                )
+                .then(response => {
+                  if (response.data.d.results.length > 0) {  
+                    proteinExpressions = response.data.d.results
+                  }
+                  return proteinExpressions
+                })
+                .catch(error => {
+                  console.log(error)
+                })
+        }
+    },
 }
 </script>
