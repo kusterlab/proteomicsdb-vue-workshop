5a5d22efd5992f7f14a32366ada89aa077e280b2
Matthew The <matthew.the@tum.de>
2022-03-22 15:44:01 +0100

    Retrieve protein id

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,16 +4,46 @@
         v-model="geneName1"
         label="Gene name 1"
         placeholder="EGFR"
+        @change="updateProteinId"
      ></v-text-field>
-     <span>{{ geneName1 }}</span>
+     <span>{{ proteinId1 }}</span>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'ProteinCorrelation',
     data: () => ({
       geneName1: "",
+      proteinId1: ""
     }),
+    methods: {
+        updateProteinId() {
+            this.getProteinId(this.geneName1).then(response => {
+                this.proteinId1 = response
+            })
+        },
+        getProteinId(geneName) {
+            let proteinId = -1
+            return axios
+                .get("https://www.proteomicsdb.org/proteomicsdb/logic/api_v2/api.xsodata/Protein", 
+                    { params : { '$filter' : `GENE_NAME eq '${geneName}' and DATABASE eq 'sp' and PARENT_PROTEIN_ID eq PROTEIN_ID and DECOY eq 0`,
+                                 '$select' : 'PROTEIN_ID', 
+                                 '$format' : 'json' } 
+                    }
+                )
+                .then(response => {
+                  if (response.data.d.results.length > 0) {  
+                    proteinId = response.data.d.results[0].PROTEIN_ID
+                  }
+                  return proteinId
+                })
+                .catch(error => {
+                  console.log(error)
+                })
+        },
+    }
 }
 </script>
